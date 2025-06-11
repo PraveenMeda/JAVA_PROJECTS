c82c187c37c6ebf89fc474ed8e8856c50522051f
PRAVEEN KUMAR MEDA <praveenmeda143@gmail.com>
2025-06-11 14:39:54 +0530

    Java

---
+++ Fibo_Recur.java
@@ -0,0 +1,18 @@
+package DAY12;
+
+public class Fibo_Recur {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=0; i<n; i++){
+            System.out.print(fibo(i)+" ");
+        }
+       
+    }
+    public static int fibo(int n){
+        if(n < 2){
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+    
+}
